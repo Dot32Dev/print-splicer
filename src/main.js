c0059037bf7c6e71efb677ea01fc24fdbb8f042c
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-08-09 13:38:07 +0800

    Show grid!

--- src/main.js
+++ src/main.js
@@ -16,7 +16,37 @@ async function file_upload() {
   let img = document.createElement("img");
   img.src = convertFileSrc(path);
   // append to body
-  document.body.appendChild(img)
+  // document.body.appendChild(img)
+  // get image container elelment
+  let img_container = document.querySelector(".image-container");
+  // append to image container
+  img_container.appendChild(img);
+
+  // const imageWidth = img.width;
+  // console.log(imageWidth)
+  // const aspectRatio = Math.sqrt(2);
+  // // const gridElementHeight = imageWidth/5 / 29.7*21;
+  // const numColumns = 5; // Number of columns
+  // const gridElementWidth = imageWidth / numColumns;
+  // console.log(gridElementWidth)
+  // const gridElementHeight = gridElementWidth / aspectRatio;
+  // const numRows = Math.ceil(img.height / gridElementHeight); // Number of rows
+  // const gridOverlay = document.querySelector(".grid-overlay");
+  // gridOverlay.style.gridTemplateColumns = `repeat(${numColumns}, ${gridElementWidth}px))`;
+  // gridOverlay.style.gridTemplateRows = `repeat(${numRows}, ${gridElementHeight}px)`;
+
+  img.onload = async () => {
+    let width = img.width;
+    let height = img.height;
+    let aspectRatio = 1/Math.sqrt(2);
+    let numColumns = 5; // Number of columns
+    let gridElementWidth = width / numColumns;
+    let gridElementHeight = gridElementWidth / aspectRatio;
+    let numRows = Math.ceil(height / gridElementHeight); // Number of rows
+    let gridOverlay = document.querySelector(".grid-overlay");
+    gridOverlay.style.gridTemplateColumns = `repeat(${numColumns}, ${gridElementWidth}px)`;
+    gridOverlay.style.gridTemplateRows = `repeat(${numRows}, ${gridElementHeight}px)`;
+  }
 
   await invoke("splice_image", { path: path });
 }
